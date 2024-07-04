52eafc79fc125f02dd87b3db031b7fad43a2b724
MVPee <mariusvanpee@hotmail.be>
2024-07-04 21:41:32 +0200

    add this when is the private attribut

--- srcs/Game.cpp
+++ srcs/Game.cpp
@@ -2,30 +2,30 @@
 
 Game::Game(int width, int height, std::string name) {
 
-    _videoMode.width = width;
-    _videoMode.height = height;
-    _window = new sf::RenderWindow(_videoMode, name, sf::Style::Default);
-    _window->setVerticalSyncEnabled(true);
+    this->_videoMode.width = width;
+    this->_videoMode.height = height;
+    this->_window = new sf::RenderWindow(_videoMode, name, sf::Style::Default);
+    this->_window->setVerticalSyncEnabled(true);
 }
 
 Game::~Game(void) {
-    delete _window;
+    delete this->_window;
 }
 
 bool Game::isRunning(void) {
-    return (_window->isOpen());
+    return (this->_window->isOpen());
 }
 
 void Game::update(void) {
-    while (_window->pollEvent(_event)) {
-        if (_event.type == sf::Event::Closed)
-            _window->close();
-        if (_event.type == sf::Event::KeyPressed && _event.key.code == sf::Keyboard::Escape)
-            _window->close();
+    while (this->_window->pollEvent(this->_event)) {
+        if (this->_event.type == sf::Event::Closed)
+            this->_window->close();
+        if (this->_event.type == sf::Event::KeyPressed && this->_event.key.code == sf::Keyboard::Escape)
+            this->_window->close();
     }
 }
 
 void Game::render(void) {
-    _window->clear();
-    _window->display();
+    this->_window->clear();
+    this->_window->display();
 }
